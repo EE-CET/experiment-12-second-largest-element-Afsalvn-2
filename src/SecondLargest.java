97451e3ebf670d4e54903877d487e5eee464acf2
Afsalvn <147943587+Afsalvn@users.noreply.github.com>
2026-04-05 10:55:50 +0530

    Add method to find second largest element in array
    Implemented method to find the second largest element in an array without sorting. Added input handling in main method to read array size and elements.

--- src/SecondLargest.java
+++ src/SecondLargest.java
@@ -1,8 +1,49 @@
+
+import java.util.Scanner;
+
 public class SecondLargest {
-    
-        // TODO: Read N
-        // TODO: Read array elements
-        // TODO: Find and print the second largest element
-        // Hint: You can do this in a single pass or two passes without sorting.
-    
+
+    // Task: Find the 2nd largest element without sorting
+    public int getSecondLargest(int[] arr, int n) {
+        // Edge case: If there are fewer than 2 elements, a second largest doesn't exist
+        if (n < 2) return -1;
+
+        int largest = -1;
+        int secondLargest = -1;
+
+        for (int i = 0; i < n; i++) {
+            // If we find a new largest, the old largest gets bumped down to second largest
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } 
+            // If it's not the largest, but it is bigger than our current second largest
+            // (and we ensure it's not a duplicate of the largest)
+            else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        return secondLargest;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Read the size of the array
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            
+            // Read the array elements
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            
+            SecondLargest solver = new SecondLargest();
+            System.out.println(solver.getSecondLargest(arr, n));
+        }
+        
+        sc.close();
+    }
 }
